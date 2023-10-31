dc5920f17333c00e69d6427cc1234dd6a7839a4d
Mounika420 <148519406+Mounika420@users.noreply.github.com>
2023-10-31 16:49:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-
+provider "aws" {
+    region ="us-east-1"  
+}
+#Resource of multiple applications
+resource "aws_instance" "multiple_apps" {
+    ami="ami-0dbc3d7bc646e8516"
+    instance_type = "t2.micro"
+    tags = {
+        Name="Raptr"
+    }
