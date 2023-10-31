5872bb277d713d0fa808cf58bdf87c6efc973dc9
Mounika420 <148519406+Mounika420@users.noreply.github.com>
2023-10-31 19:37:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 provider "aws" {
     region ="us-east-1"  
 }
-#Resource of multiple applications
-resource "aws_instance" "multiple_apps" {
-    ami="ami-0dbc3d7bc646e8516"
-    instance_type = "t2.micro"
-    tags = {
-        Name="Raptr"
-    }
+resource "aws_db_instance" "RDS_DB" {
+  identifier           = "database-2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"  
+  username             = "mounika"
+  password             = "admin_123"
+  allocated_storage    = 20
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_name = "multi_db"
 }
